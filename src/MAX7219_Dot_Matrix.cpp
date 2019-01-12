c2ae64ec1bf9b4c41159f83f9eacfb8db7653b8c
Adam Szadkowski <adam@szadkowski.info>
2019-01-12 11:34:19 +0100

    Improve code readability

--- src/MAX7219_Dot_Matrix.cpp
+++ src/MAX7219_Dot_Matrix.cpp
@@ -80,15 +80,17 @@ Usage:
 
 */
 
-// destructor
-MAX7219_Dot_Matrix::~MAX7219_Dot_Matrix() { end(); } // end of destructor
+MAX7219_Dot_Matrix::MAX7219_Dot_Matrix(const byte chips, const byte load,
+                                       const byte din, const byte clock)
+    : chips_(chips), load_(load), din_(din), clock_(clock) {}
+
+MAX7219_Dot_Matrix::~MAX7219_Dot_Matrix() { end(); }
 
 void MAX7219_Dot_Matrix::begin() {
   pinMode(load_, OUTPUT);
   digitalWrite(load_, HIGH);
 
   SPI.begin();
-  //    SPI.setClockDivider (SPI_CLOCK_DIV8);
 
   // repeatedly configure the chips in case they start up in a mode which
   // draws a lot of current
@@ -96,47 +98,43 @@ void MAX7219_Dot_Matrix::begin() {
     sendToAll(MAX7219_REG_SCANLIMIT, 7);   // show 8 digits
     sendToAll(MAX7219_REG_DECODEMODE, 0);  // use bit patterns
     sendToAll(MAX7219_REG_DISPLAYTEST, 0); // no display test
-    sendToAll(MAX7219_REG_INTENSITY, 15); // character intensity: range: 0 to 15
-    sendString("");                       // clear display
+    sendToAll(MAX7219_REG_INTENSITY, 0); // character intensity: range: 0 to 15
+    sendString("");                      // clear display
     sendToAll(MAX7219_REG_SHUTDOWN,
               1); // not in shutdown mode (ie. start it up)
   }
-} // end of MAX7219_Dot_Matrix::begin
+}
 
 void MAX7219_Dot_Matrix::end() {
   sendToAll(MAX7219_REG_SHUTDOWN, 0); // shutdown mode (ie. turn it off)
-
   SPI.end();
-} // end of MAX7219_Dot_Matrix::end
+}
 
 void MAX7219_Dot_Matrix::setIntensity(const byte amount) {
   sendToAll(MAX7219_REG_INTENSITY,
             amount & 0xF); // character intensity: range: 0 to 15
-} // end of MAX7219_Dot_Matrix::setIntensity
+}
 
-// send one byte to MAX7219
 void MAX7219_Dot_Matrix::sendByte(const byte reg, const byte data) {
   SPI.transfer(reg);
   SPI.transfer(data);
-} // end of sendByte
+}
 
 void MAX7219_Dot_Matrix::sendToAll(const byte reg, const byte data) {
   digitalWrite(load_, LOW);
   for (byte chip = 0; chip < chips_; chip++)
     sendByte(reg, data);
   digitalWrite(load_, HIGH);
-} // end of sendToAll
+}
 
 void MAX7219_Dot_Matrix::sendChar(const byte chip, const byte data) {
-  // get this character from PROGMEM
   byte pixels[8];
   for (byte i = 0; i < 8; i++)
     pixels[i] = pgm_read_byte(&MAX7219_Dot_Matrix_font[data][i]);
 
   send64pixels(chip, pixels);
-} // end of sendChar
+}
 
-// send one character (data) to position (chip)
 void MAX7219_Dot_Matrix::send64pixels(const byte chip, const byte pixels[8]) {
   for (byte col = 0; col < 8; col++) {
     // start sending
@@ -154,10 +152,9 @@ void MAX7219_Dot_Matrix::send64pixels(const byte chip, const byte pixels[8]) {
       sendByte(MAX7219_REG_NOOP, MAX7219_REG_NOOP);
     // all done!
     digitalWrite(load_, HIGH);
-  } // end of for each column
-} // end of sendChar
+  }
+}
 
-// write an entire null-terminated string to the LEDs
 void MAX7219_Dot_Matrix::sendString(const char *s) {
   byte chip;
 
@@ -167,8 +164,7 @@ void MAX7219_Dot_Matrix::sendString(const char *s) {
   // space out rest
   while (chip < (chips_))
     sendChar(chip++, ' ');
-
-} // end of sendString
+}
 
 void MAX7219_Dot_Matrix::sendSmooth(const char *s, const int pixel) {
   int len = strlen(s);
@@ -206,7 +202,5 @@ void MAX7219_Dot_Matrix::sendSmooth(const char *s, const int pixel) {
 
     // send the appropriate 8 pixels (offset will be from -7 to +7)
     send64pixels(chip, &thisChip[8 + offset]);
-
-  } // for each chip
-
-} // end of MAX7219_Dot_Matrix::sendSmooth
+  }
+}
--- src/MAX7219_Dot_Matrix.h
+++ src/MAX7219_Dot_Matrix.h
@@ -3,12 +3,23 @@
 #include <Arduino.h>
 
 class MAX7219_Dot_Matrix {
-  // pins
-  const byte chips_;
-  const byte load_;
-  const byte din_;
-  const byte clock_;
+public:
+  MAX7219_Dot_Matrix(const byte chips, const byte load, const byte din = 0,
+                     const byte clock = 0);
+  ~MAX7219_Dot_Matrix();
 
+  void begin();
+  void end();
+
+  void sendChar(const byte pos, const byte data);
+  void send64pixels(const byte pos, const byte data[8]);
+  void sendString(const char *s);
+  void sendSmooth(const char *s, const int pixel);
+  void setIntensity(const byte amount); // 0 to 15
+
+  static const byte HYPHEN = 0b0000001;
+
+private:
   void sendByte(const byte reg, const byte data);
   void sendToAll(const byte reg, const byte data);
 
@@ -21,24 +32,11 @@ class MAX7219_Dot_Matrix {
     MAX7219_REG_SCANLIMIT = 0xB,
     MAX7219_REG_SHUTDOWN = 0xC,
     MAX7219_REG_DISPLAYTEST = 0xF,
-  }; // end of enum
+  };
 
-public:
-  // constructor
-  MAX7219_Dot_Matrix(const byte chips, const byte load, const byte din = 0,
-                     const byte clock = 0)
-      : chips_(chips), load_(load), din_(din), clock_(clock) {}
-
-  ~MAX7219_Dot_Matrix(); // destructor
-  void begin();
-  void end();
-
-  void sendChar(const byte pos, const byte data);
-  void send64pixels(const byte pos, const byte data[8]);
-  void sendString(const char *s);
-  void sendSmooth(const char *s, const int pixel);
-  void setIntensity(const byte amount); // 0 to 15
-
-  static const byte HYPHEN = 0b0000001;
-
-}; // end of class MAX7219_Dot_Matrix
+private:
+  const byte chips_;
+  const byte load_;
+  const byte din_;
+  const byte clock_;
+};
